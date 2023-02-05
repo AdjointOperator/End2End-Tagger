130c9bc7901d53ce35fe657809b7bc1567b460ab
AO <adjoint_operator@protonmail.com>
2023-02-05 21:04:19 +0100

    :tada: first commit, bugs fall from the sky

---
+++ danbooru/fetch_tags.py
@@ -0,0 +1,195 @@
+import argparse
+import asyncio
+import hashlib
+import json
+import io
+import re
+
+from asyncio import gather, Semaphore
+from pathlib import Path
+from urllib.parse import urljoin
+
+import aiohttp
+import retry
+
+from lxml import html
+from PIL import Image
+from tqdm.auto import tqdm
+
+
+MD5_PREFER_FNAME = True
+LONG_SIDE = 768
+iqdb_url = "https://danbooru.iqdb.org/"
+UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'
+
+known_img_suffix = set(['jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp', 'tiff', 'tif'])
+md5_m = re.compile(r'(?<!\w)([0-9a-fA-F]{32})(?!\w)')
+pid_m = re.compile(r'(?<!\w)(\d+_p\d+)(?!\w)')
+
+
+def img_dirwalk(path: Path):
+    for fname in path.iterdir():
+        if fname.is_dir():
+            yield from img_dirwalk(fname)
+        else:
+            if fname.suffix[1:] in known_img_suffix:
+                yield fname
+
+
+def img_generator(path: Path, retry_nomatch: bool = False):
+    for p in img_dirwalk(path):
+        if p.with_suffix('.json').exists():
+            continue
+        if (not retry_nomatch) and p.with_suffix('.nomatch').exists():
+            continue
+        yield p
+
+
+def get_md5(path: Path):
+    if MD5_PREFER_FNAME:
+        m = md5_m.search(path.name)
+        if m:
+            return m.group(1).lower()
+    with open(path, 'rb') as f:
+        return hashlib.md5(f.read()).hexdigest()
+
+
+def get_pidstr(path: Path):
+    m = pid_m.search(path.name)
+    if m:
+        return m.group(1)
+    return None
+
+
+@retry.retry(aiohttp.ClientError, tries=5, delay=1, backoff=1)
+@retry.retry(aiohttp.ClientConnectionError, tries=5, delay=1, backoff=1)
+async def md5_lookup(sess: aiohttp.ClientSession, path: Path, sema: Semaphore):
+    md5 = get_md5(path)
+    await sema.acquire()
+    r = await sess.get('https://danbooru.donmai.us/posts.json', params={'tags': f'md5:{md5}'})
+    r.raise_for_status()
+    js = await r.json()
+    sema.release()
+    if len(js) == 0:
+        return None
+    return js[0]
+
+
+@retry.retry(aiohttp.ClientError, tries=5, delay=1, backoff=1)
+@retry.retry(aiohttp.ClientConnectionError, tries=5, delay=1, backoff=1)
+async def pidstr_lookup(sess: aiohttp.ClientSession, path: Path, sema: Semaphore):
+    pid_str = get_pidstr(path)
+    if not pid_str:
+        return None
+    pid, _ = pid_str.split('_')
+    await sema.acquire()
+    r = await sess.get('https://danbooru.donmai.us/posts.json', params={'tags': f'pixiv:{pid}'})
+    r.raise_for_status()
+    js = await r.json()
+    sema.release()
+    for j in js:
+        if pid_str in j['source']:
+            return j
+
+
+@retry.retry(aiohttp.ClientError, tries=5, delay=1, backoff=1)
+@retry.retry(aiohttp.ClientConnectionError, tries=5, delay=1, backoff=1)
+async def iqdb_lookup(
+    sess: aiohttp.ClientSession,
+    path: Path,
+    sema_iqdb: Semaphore,
+    sema_danbooru: Semaphore
+):
+    img = Image.open(path)
+    h, w = img.size
+    r = LONG_SIDE / max(h, w)
+    if r < 1:
+        nh, nw = int(h * r), int(w * r)
+        img = img.resize((nw, nh), Image.Resampling.BICUBIC)
+    img_bytes = io.BytesIO()
+    fmt = 'JPEG' if img.mode == 'RGB' else 'PNG'
+    img.save(img_bytes, format=fmt, quality=90)
+    files = {'file': img_bytes.getvalue()}
+    await sema_iqdb.acquire()
+    r = await sess.post(iqdb_url, data=files)
+    ht = html.fromstring(await r.text())
+    sema_iqdb.release()
+    matched = ht.xpath('//parent::table[tr/th/text()="Best match"]')
+    if not matched:
+        return None
+    similarity = matched[0].xpath('.//td[contains(text(), "similarity")]/text()')[0]
+    similarity = float(similarity.strip().split('%')[0])
+    danbooru_url = matched[0].xpath('.//a')[0].attrib['href']
+    danbooru_url = urljoin(iqdb_url, danbooru_url) + '.json'
+    await sema_danbooru.acquire()
+    r = await sess.get(danbooru_url)
+    j = await r.json()
+    sema_danbooru.release()
+    j['similarity'] = similarity
+    j['lookup_method'] = 'iqdb'
+    return j
+
+
+async def lookup(sess: aiohttp.ClientSession, path: Path, sema_iqdb: Semaphore, sema_danbooru: Semaphore):
+    j = await pidstr_lookup(sess, path, sema_danbooru)
+    if j:
+        return j
+    j = await md5_lookup(sess, path, sema_danbooru)
+    if j:
+        return j
+    j = await iqdb_lookup(sess, path, sema_iqdb, sema_danbooru)
+    if j:
+        return j
+    return None
+
+
+async def lookup_and_save(
+    sess: aiohttp.ClientSession,
+    path: Path,
+    sema_iqdb: Semaphore,
+    sema_danbooru: Semaphore,
+    pbar: tqdm | None = None
+):
+    j = await lookup(sess, path, sema_iqdb, sema_danbooru)
+    if j:
+        with open(path.with_suffix('.json'), 'w') as f:
+            json.dump(j, f, indent=2)
+    else:
+        with open(path.with_suffix('.nomatch'), 'w') as f:
+            pass
+    if pbar is not None:
+        pbar.update(1)
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', '-p', type=Path)
+    parser.add_argument('--retry-nomatch', action='store_true')
+    parser.add_argument('--md5-prefer-fname', action='store_false')
+    parser.add_argument('--danbooru-concurrency', type=int, default=2)
+    parser.add_argument('--iqdb-concurrency', type=int, default=2)
+    parser.add_argument('--iqdb-long-side', type=int, default=768)
+    return parser.parse_args()
+
+
+async def main(path: str | Path, iqdb_concurrency=2, danbooru_concurrency=2, retry_nomatch=False):
+    imgs = list(img_generator(Path(path), retry_nomatch=retry_nomatch))
+    if not imgs:
+        print('All metadata fetched.')
+        return
+    sema_iqdb = Semaphore(iqdb_concurrency)
+    sema_danbooru = Semaphore(danbooru_concurrency)
+    sess = aiohttp.ClientSession(headers={'User-Agent': UA})
+
+    pbar = tqdm(total=len(imgs))
+    jobs = [lookup_and_save(sess, p, sema_iqdb, sema_danbooru, pbar) for p in imgs]
+    await gather(*jobs)
+    await sess.close()
+
+
+if __name__ == '__main__':
+
+    args = get_args()
+    LONG_SIDE = args.iqdb_long_side
+    MD5_PREFER_FNAME = args.md5_prefer_fname
+    asyncio.run(main(args.path, args.iqdb_concurrency, args.danbooru_concurrency, args.retry_nomatch))
---
+++ e2e_tagger.py
@@ -0,0 +1,323 @@
+import sys
+with open('.env') as f:
+    for line in f:
+        key, value = line.strip().split('=')
+        sys.path.append(value)
+
+import asyncio
+import argparse
+import json
+import yaml
+
+from dataclasses import dataclass, field
+from pathlib import Path
+
+import numpy as np
+
+from prediction import get_predictions
+from utils import sigmoid
+
+from danbooru.fetch_tags import main as fetch_danbooru_tag
+from danbooru.fetch_tags import known_img_suffix, img_dirwalk
+
+
+@dataclass
+class TaggerConfig:
+    keep_catagories: tuple[str] = ('general',)  # ('general', 'character', 'copyright', 'artist', 'meta')
+    tagger_threshold1: float = 0.3
+    tagger_threshold2: float = 0.55
+    model_path: str | Path = 'src/Augmented-DDTagger/models/wd-v1-4-swinv2-tagger-v2'
+    combine_mode: str = 'AND'
+    fallback: bool = True
+
+    def __post_init__(self):
+        self.model_path = Path(self.model_path)
+        self._keep_catagories = set(self.keep_catagories)
+
+
+with open('tag_groups_flattened.json') as f:
+    tag_groups = json.load(f)
+
+raw_tags = np.loadtxt('src/Augmented-DDTagger/models/wd-v1-4-swinv2-tagger-v2/selected_tags.csv', dtype=str, delimiter=',')[1:]
+
+all_tags = raw_tags[:, 1].tolist()
+count = raw_tags[:, 3].astype(np.int32)
+category = raw_tags[:, 2].astype(np.int32)
+
+
+MASK = np.zeros_like(count, dtype=bool)
+mask_general = (category == 0)
+mask_character = (category == 4)
+mask_meta = (category == 9)
+
+
+def get_danbooru_tags_dict(img_path: Path) -> dict[str, list[str]] | None:
+    if img_path.with_suffix('.json').exists():
+        with open(img_path.with_suffix('.json')) as f:
+            db_json = json.load(f)
+        tags_dict = dict(
+            general=db_json['tag_string_general'].split(),
+            character=db_json['tag_string_character'].split(),
+            copyright=db_json['tag_string_copyright'].split(),
+            artist=db_json['tag_string_artist'].split(),
+            meta=db_json['tag_string_meta'].split(),
+        )
+    else:
+        tags_dict = None
+    return tags_dict
+
+
+def get_pred_dict(img_path: Path, score: np.ndarray, tags: list[str]) -> dict[str, np.float32]:
+    mask = MASK & (score > 0.001)
+    indices = np.where(mask)[0]
+    order = np.argsort(score[indices])[::-1]
+    return {tags[i]: score[i] for i in indices[order]}
+
+
+def get_original_label(img_path: Path) -> str | None:
+    if img_path.with_suffix('.txt').exists():
+        with open(img_path.with_suffix('.txt')) as f:
+            return f.read().strip()
+
+# """
+# Args:
+#     danbooru_tags: category [general, meta, character, artist] -> tag with _
+#     tagger_preds: tag -> confidence. Sorted by confidence in descending
+#     original_tags: original tags
+# """
+
+
+def combine_dd_first(config: TaggerConfig, danbooru_tags: dict[str, str] | None, tagger_preds: dict[str, float], original_label: str):
+    if danbooru_tags:
+        return [tag for cate, taglist in danbooru_tags.items() for tag in taglist if cate in config._keep_catagories]
+    elif not config.fallback:
+        raise ValueError('DD_ONLY is True but no danbooru tags found')
+    return [tag for tag, confid in tagger_preds.items() if confid > config.tagger_threshold2]
+
+
+def combine_AND(config: TaggerConfig, danbooru_tags: dict[str, str] | None, tagger_preds: dict[str, float], original_label: str):
+    if danbooru_tags:
+        dd_tags = set([tag for cate, taglist in danbooru_tags.items() for tag in taglist if cate in config._keep_catagories])
+        return_tags = []
+        for tag, confid in tagger_preds.items():
+            if confid < config.tagger_threshold1:
+                break
+            if tag in dd_tags:
+                return_tags.append(tag)
+        return return_tags
+    return [tag for tag, confid in tagger_preds.items() if confid > config.tagger_threshold2]
+
+
+def combine_OR(config: TaggerConfig, danbooru_tags: dict[str, str] | None, tagger_preds: dict[str, float], original_label: str):
+    tags = [tag for tag, confid in tagger_preds.items() if confid > config.tagger_threshold2]
+    if danbooru_tags:
+        dd_tags = set([tag for cate, taglist in danbooru_tags.items() for tag in taglist if cate in config._keep_catagories])
+        tags = list(dd_tags | set(tags))
+    return tags
+
+
+def combine_tagger_first(config: TaggerConfig, danbooru_tags: dict[str, str] | None, tagger_preds: dict[str, float], original_label: str):
+    return [tag for tag, confid in tagger_preds.items() if confid > config.tagger_threshold2]
+
+
+def combine(config: TaggerConfig, pred_dict: dict[str, np.float32], danbooru_tag_dict: dict[str, list[str]] | None, original_label: str | None) -> list[str]:
+    assert config.combine_mode in ('AND', 'OR', 'DD-first', 'TAGGER-first')
+
+    ret_dict = {
+        'AND': combine_AND,
+        'OR': combine_OR,
+        'DD-first': combine_dd_first,
+        'TAGGER-first': combine_tagger_first,
+    }
+    return ret_dict[config.combine_mode](config, danbooru_tag_dict, pred_dict, original_label)  # type: ignore
+
+
+def an_astolfo_is_1girl(all_tags: list[str]):
+    """Generate gender related tags according to the tagger's prediction. If it looks like 1girl, then it is 1girl."""
+
+    gp1 = ['1girl', '2girls', '3girls', '4girls', '5girls', '6+girls']
+    gp2 = ['1boy', '2boys', '3boys', '4boys', '5boys', '6+boys']
+    gp3 = ['no_humans']
+    rm = ['genderswap', 'genderswap_(mtf)', 'genderswap_(ftm)', 'ambiguous_gender'] + gp1 + gp2
+    rm = set(rm)
+    groups = [gp1, gp2, gp3]
+    gp_indices = [np.array([all_tags.index(t) for t in g]) for g in groups]
+
+    def make_astolfo_girl(config: TaggerConfig, combined_tags: list[str], score: np.ndarray):
+        gp_scores = [score[idxs] for idxs in gp_indices]
+        gp_scores_max = [gp_score.max() for gp_score in gp_scores]
+        top_gp = np.argmax(gp_scores_max)
+        top_loc = np.argmax(score[gp_indices[top_gp]])
+        keep = [groups[top_gp][top_loc]]
+        for i in range(len(groups)):
+            if i == top_gp:
+                continue
+            if gp_scores_max[i] > config.tagger_threshold2:
+                keep.append(groups[i][np.argmax(gp_scores[i])])
+        for tag in combined_tags:
+            if tag not in rm:
+                keep.append(tag)
+        return keep
+    return make_astolfo_girl
+
+
+useless_tags = set(
+    ['virtual_youtuber'] +
+    [tag for tag in all_tags if 'alternate_' in tag]
+)
+
+
+def rm_useless_tags(combined_tags):
+    return [tag for tag in combined_tags if tag not in useless_tags]
+
+
+make_astolfo_girl = an_astolfo_is_1girl(all_tags)
+
+
+def process(
+    config: TaggerConfig,
+    pred_dict: dict[str, np.float32],
+    scores: np.ndarray,
+    danbooru_tag_dict: dict[str, list[str]] | None,
+    original_label: str | None,
+):
+    tags = combine(config, pred_dict, danbooru_tag_dict, original_label)
+    tags = make_astolfo_girl(config, tags, scores)
+    tags = rm_useless_tags(tags)
+    return tags
+
+
+class Postprocess:
+    def __init__(
+        self,
+        rm: list[str] | None = None,
+        replace: list[tuple[str, str]] | None = None,
+        prepend: list[str] | None = None,
+        append: list[str] | None = None,
+        must_have: list[str] | None = None,
+    ):
+        self.rm = set(rm) if rm else set()
+        self.replace = {k: v for k, v in replace} if replace else {}
+        _prepend = set(prepend) if prepend else set()
+        self.append = list(set(append)) if append else []
+        if must_have:
+            for t in must_have:
+                self.rm.add(t)
+                _prepend.add(t)
+        self.prepend = list(_prepend)
+
+    def __call__(self, tags: list[str]) -> list[str]:
+        filtered_tags = []
+        for t in tags:
+            if t in self.rm:
+                continue
+            if t in self.replace:
+                t = self.replace[t]
+            filtered_tags.append(t)
+        return self.prepend + filtered_tags + self.append
+
+    def update(self, p: Path) -> 'Postprocess':
+        if not p.exists():
+            return self
+        with open(p, 'r') as f:
+            other: dict = yaml.load(f, Loader=yaml.FullLoader)
+        if not other:
+            return self
+        for k, v in other.items():
+            if v.__class__ is not list:
+                other[k] = [v]
+
+        rm = list(self.rm) + other.get('rm', [])
+        replace = list(self.replace.items()) + [tuple(v) for v in other.get('replace', [])]
+        prepend = list(self.prepend) + other.get('prepend', [])
+        append = list(self.append) + other.get('append', [])
+        must_have = list(other.get('must_have', []))
+        return Postprocess(rm, replace, prepend, append, must_have)
+
+
+def _img_dirwalk_with_postprocessor(path: Path, postprocessor: Postprocess):
+    for p in path.iterdir():
+        if p.is_dir():
+            _postprocessor = postprocessor.update(p / 'rules.yaml')
+            if not p.name.startswith('_'):
+                _postprocessor.prepend.append(p.name)
+            yield from _img_dirwalk_with_postprocessor(p, _postprocessor)
+        else:
+            if p.suffix[1:] in known_img_suffix:
+                yield p, postprocessor
+
+
+def img_dirwalk_with_postprocessor(path: Path | str):
+    path = Path(path)
+    postprocess = Postprocess(prepend=[path.name]) if not path.name.startswith('_') else Postprocess()
+    yield from _img_dirwalk_with_postprocessor(path, postprocess.update(path / 'rules.yaml'))
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', '-p', type=str, required=True)
+    parser.add_argument('--model_path', type=str, required=False, default=None, help='Path to model for AugDD')
+    parser.add_argument('--batch_size', type=int, default=16, help='Batch size for inference in AugDD')
+    parser.add_argument('--backend', type=str, default='WD-SwinV2', help='Backend model to use in AugDD')
+    parser.add_argument('--nproc', type=int, default=-1, help='Number of processes to use for AugDD. -1 means all')
+    parser.add_argument('--max_chunk', type=int, default=16, help='Maximum number of batches to process before one save in AugDD')
+    parser.add_argument('--danbooru-concurrency', type=int, default=2, help='Number of concurrent requests to Danbooru')
+    parser.add_argument('--iqdb-concurrency', type=int, default=2, help='Number of concurrent requests to iqdb')
+    parser.add_argument('--retry-nomatch', action='store_true', help='Retry images that have no matches on iqdb')
+    parser.add_argument('--keep-catagories', nargs='+', default=['general'], help='Available: general, character, artist, copyright, meta')
+    parser.add_argument('--threshold1', '-t1', type=float, default=0.3, help='Tagger threshold for AND mode')
+    parser.add_argument('--threshold2', '-t2', type=float, default=0.55, help='Tagger threshold for OR and fallback mode')
+    parser.add_argument('--combine-mode', '-cm', type=str, default='AND', choices=['AND', 'OR', 'TAGGER-first', 'DD-first'])
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+
+    args = get_args()
+    if not args.model_path:
+        args.model_path = 'src/Augmented-DDTagger/models/wd-v1-4-swinv2-tagger-v2'
+        print('Using default model path:', args.model_path)
+
+    if 'general' in args.keep_catagories:
+        MASK |= mask_general
+    if 'character' in args.keep_catagories:
+        MASK |= mask_character
+    if 'meta' in args.keep_catagories:
+        MASK |= mask_meta
+
+    config = TaggerConfig(
+        keep_catagories=tuple(args.keep_catagories),
+        tagger_threshold1=args.threshold1,
+        tagger_threshold2=args.threshold2,
+        model_path=args.model_path,
+        combine_mode=args.combine_mode,
+        fallback=True,
+    )
+
+    path = Path(args.path)
+
+    print('Running AugDD...')
+    scores, rel_paths = get_predictions(
+        model_path=args.model_path,
+        root_path=path,
+        batch_size=args.batch_size,
+        backend=args.backend,
+        nproc=args.nproc,
+        max_chunk=args.max_chunk,
+    )
+    scores = sigmoid(np.float32(scores))
+
+    score_dicts = {k: v for k, v in zip(rel_paths, scores)}  # type: ignore $ img_path -> confidence
+
+    print('Fetching Danbooru tags...')
+    asyncio.run(fetch_danbooru_tag(args.path, args.iqdb_concurrency, args.danbooru_concurrency, args.retry_nomatch))
+
+    for img_path, postproc in img_dirwalk_with_postprocessor(path):
+        danbooru_tag_dict = get_danbooru_tags_dict(img_path)
+        scores = score_dicts[str(img_path.relative_to(path))]
+        pred_dict = get_pred_dict(img_path, scores, all_tags)
+        original_label = get_original_label(img_path)
+        tags = process(config, pred_dict, scores, danbooru_tag_dict, original_label)
+        tags = postproc(tags)
+        with open(img_path.with_suffix('.txt'), 'w') as f:
+            f.write(' '.join(tags))
